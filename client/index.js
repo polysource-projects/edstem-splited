e03fc0c6fbd24a474db53af87e0e98d38701a506
Simon <androz2091@gmail.com>
2024-03-24 09:14:50 +0100

    :pushpin: Bump 1.6

--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SplitED
 // @namespace    violentmonkey
-// @version      1.5
+// @version      1.6
 // @description  SplitED
 // @author       violentmonkey
 // @match        https://edstem.org/*
